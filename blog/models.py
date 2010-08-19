683f9fc2bbda8a00c41df3c02beb57a556d1ec26
edgar <edgar@spooky.local>
2010-08-18 21:35:55 -0300

    Adicionado o atributo author no post

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	body = models.TextField()
-	publish = models.DateTimeField()
+	author = models.ForeignKey('auth.User', blank = True, null = False)
+	published = models.DateTimeField()
 
 	class Admin :
 		pass
