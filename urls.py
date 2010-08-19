a5b77bbcaf5b996d6b4e4a391327e32ffd405e65
edgar <edgar@spooky.local>
2010-08-18 21:34:38 -0300

    Adicionada as urls de media e js

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+	(r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'media/js'}),
+	(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	url(r'^$', 'ecosite.blog.views.post_index', name='post-index'),
 )
