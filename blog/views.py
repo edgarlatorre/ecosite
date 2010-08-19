0d3139233c2a5e9714d9cf3ecf670eef5895beff
edgar <edgar@spooky.local>
2010-08-18 21:35:11 -0300

    Criado o post_index

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+from django.template import RequestContext
+from ecosite.blog.models import Post
+
+def post_index(request):
+	posts = Post.objects.order_by('-published')
+	return render_to_response('blog/index.html', {'posts':posts}, context_instance = RequestContext(request))
+
+
+
+
