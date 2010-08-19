ed5fb44188798c186d7df936e80b3cd395d6b2dd
edgar <edgar@spooky.local>
2010-08-19 09:44:49 -0300

    Alteracoes para adicionar as tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from django import forms
 from models import Post
+from ecosite.tags import aplicar_tags
 
-admin.site.register(Post)
+
+class FormPost(forms.ModelForm) :
+	class Meta:
+		model = Post
+		
+	tags = forms.CharField(max_length=30, required=False)
+	
+	def __init__(self, *args, **kwargs) :
+		super(FormPost, self).__init__(*args, **kwargs)
+
+		if self.instance.id:
+			self.initial['tags'] = tags_to_object(self.instance)
+	
+class AdminPost(ModelAdmin) :
+	form = FormPost
+	
+	def save_model(self, request, obj, form, change) :
+		super(AdminPost, self).save_model(request, obj, form, change)
+		apply_tags(obj, form.cleaned_data['tags'])
+		
+admin.site.register(Post, AdminPost)
--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 
 class Tag(models.Model):
 	name = models.CharField(max_length=30, unique=True)
@@ -6,3 +7,10 @@ class Tag(models.Model):
 	def __unicode__(self):
 		return self.name
 		
+class TagItem(models.Model) :
+	class Meta :
+		unique_together = ('tag', 'content_type', 'object_id')
+		
+	tag = models.ForeignKey('Tag')
+	content_type = models.ForeignKey(ContentType)
+	object_id = models.PositiveIntegerField(db_index=True)
