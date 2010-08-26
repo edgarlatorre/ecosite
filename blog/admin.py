944aba827e571866418d98087dde3a26204ff298
edgar <edgar@spooky.local>
2010-08-25 21:02:42 -0300

    Adicionada flatpages e template

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.admin import FlatPageAdmin
 from django import forms
+from django.conf import settings
 from models import Post
 from ecosite.tags import apply_tags, tags_to_object
 
 
 class FormPost(forms.ModelForm) :
 	class Media:
-		js = ('/js/tiny_mce/tiny_mce.js', '/js/textareas.js')
+		js = ('%s/js/tiny_mce/tiny_mce.js' % settings.MEDIA_URL, '%s/js/textareas.js' % settings.MEDIA_URL)
     
 	class Meta:
 		model = Post
@@ -21,10 +24,16 @@ class FormPost(forms.ModelForm) :
 			self.initial['tags'] = tags_to_object(self.instance)
 	
 class AdminPost(ModelAdmin) :
-	form = FormPost
-	
+	class Media:
+		js = ('/js/tiny_mce/tiny_mce.js', '/js/textareas.js')
+		
+	form = FormPost	
 	def save_model(self, request, obj, form, change) :
 		super(AdminPost, self).save_model(request, obj, form, change)
 		apply_tags(obj, form.cleaned_data['tags'])
 		
+class CustomFlatPageAdmin(FlatPageAdmin):
+	class Media:
+		js = ('%s/js/tiny_mce/tiny_mce.js' % settings.MEDIA_URL, '%s/js/textareas.js' % settings.MEDIA_URL)
+
 admin.site.register(Post, AdminPost)
